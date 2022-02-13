1bfe5d3dba7b2aa09e6e89c7828357ff0dddd257
VissaMoutafis <sdi1800119@di.uoa.gr>
2022-02-13 14:09:13 +0200

    adding notes from last meeting

--- Code/mia_v2/label_only.py
+++ Code/mia_v2/label_only.py
@@ -2,15 +2,18 @@
 # in a black-box label only fashion
 # ~Written by Vissarion Moutafis~ for Undergraduate Thesis
 import numpy as np
-from mia_v1.attack_model import DefaultAttackModel
-from mia_v1.utilities import create_rotates, create_translates, apply_augment
+from mia_v2.attack_model import DefaultAttackModel
+from mia_v2.utilities import create_rotates, create_translates, apply_augment
 from tensorflow.keras import layers
 
 """ API of model to get predictions : returns labels only"""
+
+
 def target_predict(model, X):
-  prob = layers.Softmax()
-  ret = prob(model.predict(X)).numpy()
-  return np.apply_along_axis(np.argmax, 1, ret).reshape((-1, 1))
+    prob = layers.Softmax()
+    ret = prob(model.predict(X)).numpy()
+    return np.apply_along_axis(np.argmax, 1, ret).reshape((-1, 1))
+
 
 """ 
 Returns a matrix of shape (n_samples, 2*r+4*d+2)
@@ -20,6 +23,8 @@ Returns a matrix of shape (n_samples, 2*r+4*d+2)
 @param r: rotates
 @param d: translates
 """
+
+
 def augmented_queries(model, X, y_pred, r=3, d=1):
     # create perturbations
     rotates = create_rotates(r)
@@ -28,7 +33,8 @@ def augmented_queries(model, X, y_pred, r=3, d=1):
     X_attack = None
     for rot in rotates:
         # skip the no rotation augment
-        if rot == 0.0: continue
+        if rot == 0.0:
+            continue
         #  create perturbed image
         X_perturbed = apply_augment(X, rot, 'r')
         # return query line
@@ -43,7 +49,8 @@ def augmented_queries(model, X, y_pred, r=3, d=1):
 
     for tra in translates:
         # skip the no translation augment
-        if tra == (0, 0, 0, 0): continue
+        if tra == (0, 0, 0, 0):
+            continue
         X_perturbed = apply_augment(X, tra, 'd')
         # return query line
         y_perturbed = target_predict(model, X_perturbed)
@@ -65,10 +72,12 @@ class LabelOnlyAttackModel(DefaultAttackModel):
     @param X_attack_dim: the dimensions of the attack dataset's instances (i.e. in confidence vector we use (n_classes,))
     @param _optimizer: optimizer to use in attack-model fitting
     """
+
     def __init__(self, shadow_batch, n_classes, f_attack_builder):
-        self.r = 2 # default values
-        self.d = 1 #default values
-        super(LabelOnlyAttackModel, self).__init__(shadow_batch, n_classes, f_attack_builder)
+        self.r = 2  # default values
+        self.d = 1  # default values
+        super(LabelOnlyAttackModel, self).__init__(
+            shadow_batch, n_classes, f_attack_builder)
 
     """
     helper to prepare a batch of shadow data into a batch of attack data
@@ -80,26 +89,30 @@ class LabelOnlyAttackModel(DefaultAttackModel):
     @param in_D: boolean-> True if data are in the model's training dataset, otherwise False
     
     """
+
     def prepare_batch(self, model, X, y, in_D):
         # decide membership
-        y_member = np.ones(shape=(y.shape[0], 1)) if in_D else np.zeros(shape=(y.shape[0], 1))
+        y_member = np.ones(shape=(y.shape[0], 1)) if in_D else np.zeros(
+            shape=(y.shape[0], 1))
 
-        # get the y_pred 
+        # get the y_pred
         prob = layers.Softmax()
         ret = prob(model.predict(X)).numpy()
         y_pred = np.apply_along_axis(np.argmax, 1, ret).reshape((-1, 1))
-        perturbed_queries_res = augmented_queries(model, X, y_pred, self.r, self.d)
-        
-        # return an instance <actual class, predicted class, perturbed_queries_res from shadow models, 'in'/'out' D_target membership> 
+        perturbed_queries_res = augmented_queries(
+            model, X, y_pred, self.r, self.d)
+
+        # return an instance <actual class, predicted class, perturbed_queries_res from shadow models, 'in'/'out' D_target membership>
         return np.concatenate((y.reshape(-1, 1), y_pred, perturbed_queries_res, y_member), axis=1)
-    
+
     """
     Fit the attack model, but first provide with perturbation params to generate the proper ammount of features per instance (given as X_inpt_dim in __init__)
     @param r: rotates
     @param d: translates
     """
+
     def fit(self, r=2, d=1, epochs=100):
-        #set up r, d before fit
+        # set up r, d before fit
         self.r = r
         self.d = d
         super(LabelOnlyAttackModel, self).fit(epochs=epochs)
